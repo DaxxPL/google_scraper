1ce8e6411fc8dcfdc724167c47902d2b746b69d6
Mikołaj Henklewski <mikolaj.henklewski@wext.it>
2019-07-08 14:49:23 +0200

    updated tests

--- queries/tests.py
+++ queries/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from django_webtest import WebTest
 
 
 class SearchViewTest(TestCase):
@@ -16,3 +17,25 @@ class SearchViewTest(TestCase):
         response = self.client.get(reverse('find'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'queries/query_form.html')
+
+
+class SearchViewFormTest(WebTest):
+
+    def test_sending_not_filled_form(self):
+        form = self.app.get(reverse('find')).form
+        response = form.submit()
+        self.assertContains(response, "This field is required.")
+
+    def test_sending_filled_form_but_negative_number_timeout(self):
+        form = self.app.get(reverse('find')).form
+        form['query'] = 'foo'
+        form['timeout'] = -4
+        response = form.submit()
+        self.assertContains(response, "Ensure this value is greater than or equal to 0.0")
+
+    def test_sending_filled_form_but_string_timeout(self):
+        form = self.app.get(reverse('find')).form
+        form['query'] = 'foo'
+        form['timeout'] = 'bar'
+        response = form.submit()
+        self.assertContains(response, "Enter a number.")
