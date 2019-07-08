6b3a4549051ff688a7c3c1d216cf20f9f0d69094
Mikołaj Henklewski <mikolaj.henklewski@wext.it>
2019-07-08 10:22:45 +0200

    secret key in env variable

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ FROM python:3.7
 # The enviroment variable ensures that the python output is set straight
 # to the terminal with out buffering it first
 ENV PYTHONUNBUFFERED 1
-ENV DJANGO_SETTINGS_MODULE=google_scraper.settings
+
 
 # create root directory for our project in the container
 RUN mkdir /gog
--- google_scraper/settings.py
+++ google_scraper/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qwg^5*16#h4ty*(hk^qs^@njm-sgyk!ts3y8-wc8ui!$bx5icq'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
